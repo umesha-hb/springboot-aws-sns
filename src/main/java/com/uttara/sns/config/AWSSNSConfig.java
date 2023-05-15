af15ce70bd68c1cef773f69688b8ead56cde1860
umesha-hb <umesha.kit@gmail.com>
2023-05-15 18:01:38 +0530

    first commit

---
+++ src/main/java/com/uttara/sns/config/AWSSNSConfig.java
@@ -0,0 +1,26 @@
+package com.uttara.sns.config;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class AWSSNSConfig {
+    public static final String SECRET_KEY = "+dL8duEfu7OLAoKC4Gb/4OyVYPURlNPLo0dQhG0/";
+    public static final String ACCESS_KEY = "AKIA2PG54BRQD76XJWPM";
+
+
+
+    @Primary
+    @Bean
+    public AmazonSNSClient getSnsClient() {
+        return (AmazonSNSClient) AmazonSNSClientBuilder.standard().withRegion(Regions.AP_SOUTH_1)
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY,
+                        SECRET_KEY)))
+                .build();
+    }
+}
